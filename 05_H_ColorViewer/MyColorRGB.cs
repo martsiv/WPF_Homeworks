1b30412d83608e2730d53e2084326e9bd97193a1
martsiv <mykola.martsiv@gmail.com>
2023-06-12 22:26:42 +0300

    Added homework #5

---
+++ 05_H_ColorViewer/MyColorRGB.cs
@@ -0,0 +1,33 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+
+namespace _06._1_L_MVVM
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MyColorRGB 
+    {
+        public short Alpha { get; set; }
+        public short Red { get; set; }
+        public short Green { get; set; }
+        public short Blue { get; set; }
+        public MyColorRGB()
+        {
+            Alpha = 0;
+            Red = 0;
+            Green = 0;
+            Blue = 0;
+        }
+        [DependsOn("Alpha", "Red", "Green", "Blue")]
+        public Color RGB => Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue);
+        public override string ToString() //to hexColor as string
+        {
+            return $"#{Alpha.ToString("X2")}{Red.ToString("X2")}{Green.ToString("X2")}{Blue.ToString("X2")}";
+        }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ 05_H_ColorViewer/VievModel.cs
@@ -0,0 +1,53 @@
+﻿using _06._1_L_MVVM.Helper;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace _06._1_L_MVVM
+{
+    [AddINotifyPropertyChangedInterface]
+    public class VievModel
+    {
+        public ObservableCollection<Color> colors = new ObservableCollection<Color>();
+        public MyColorRGB Color2 { get; set; }
+        private readonly RelayCommand addColor;
+        private readonly RelayCommand removeColor;
+        private Color? selectedColor;
+        public Color? SelectedColor 
+        { 
+            get => selectedColor; 
+            set 
+            { 
+                selectedColor = value; 
+                Color2.Red = value.Value.R; 
+                Color2.Alpha = value.Value.A;
+                Color2.Green = value.Value.G;
+                Color2.Blue = value.Value.B;
+            } 
+        }
+        public IEnumerable<Color> MyColors => colors;
+        public ICommand AddColor => addColor;
+        public ICommand RemoveColor => removeColor;
+        public VievModel()
+        {
+            Color2 = new MyColorRGB() { Alpha = 100, Blue = 23, Green = 46, Red = 120 };
+
+            addColor = new((o) => Add(), (с) => !colors.Contains(Color2.RGB));
+            removeColor = new((o) => Remove(), (o) => selectedColor != null);
+        }
+        public void Add()
+        {
+            colors.Add(Color2.RGB);
+        }
+        public void Remove() 
+        {
+            colors.Remove(selectedColor.Value);
+        }
+    }
+}
